9231cfd54ae509ca2d52a875a2078237093e50d1
Jasmine Young <40215583+jasmineaimee@users.noreply.github.com>
2020-04-13 09:39:15 -0600

    pushing ar to test in ios

---
+++ App/Assets/Scripts/ImageTracking.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.XR;
+using UnityEngine.XR.ARFoundation;
+
+public class ImageTracking : MonoBehaviour
+{
+   public ARTrackedImageManager arTrackedImageManager;
+   public Text debugText;
+
+    void OnEnable()
+    {
+        arTrackedImageManager.trackedImagesChanged += OnImageChanged;
+    }
+    void OnDisable()
+    {
+        arTrackedImageManager.trackedImagesChanged -= OnImageChanged;
+    }
+
+    public void OnImageChanged(ARTrackedImagesChangedEventArgs args)
+    {
+        foreach(var trackedImage in args.added)
+        {
+            if(trackedImage.name == "washHandsImg")
+            {
+                debugText.text = trackedImage.name;
+            }
+        }
+    } 
+}
