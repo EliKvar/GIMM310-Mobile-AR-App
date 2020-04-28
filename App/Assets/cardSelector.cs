89f4d5bb7a56f5b7db8c554412717a76eae07d5c
Mothskee <54814906+Mothskee@users.noreply.github.com>
2020-04-27 21:56:36 -0600

    Learning Animator
    Cleaned up some of the unnessessary stuff.

---
+++ App/Assets/cardSelector.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cardSelector : MonoBehaviour
+{
+
+    Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Z))
+        {
+            animator.SetBool("Zpress", true);
+        }
+        else if (Input.GetKey(KeyCode.X))
+        {
+            animator.SetBool("Xpress", true);
+        }
+        else if (Input.GetKey(KeyCode.C))
+        {
+            animator.SetBool("Cpress", true);
+        }
+        else if (Input.GetKey(KeyCode.V))
+        {
+            animator.SetBool("Vpress", true);
+        }
+        else if (Input.GetKey(KeyCode.Z))
+        {
+            animator.SetBool("Bpress", true);
+        }
+        else if (Input.GetKey(KeyCode.None))
+        {
+            animator.SetBool("Zpress", false);
+            animator.SetBool("Xpress", false);
+            animator.SetBool("Cpress", false);
+            animator.SetBool("Vpress", false);
+            animator.SetBool("Bpress", false);
+        }
+        
+
+    }
+}
